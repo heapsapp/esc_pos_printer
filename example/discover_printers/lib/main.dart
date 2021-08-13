275d8854ff6b2a5de65214272f8af6738c5d92df
tnovoselec <tomislav.novoselec@gmail.com>
2021-08-13 14:51:17 +0200

    flutter null safety

--- example/discover_printers/lib/main.dart
+++ example/discover_printers/lib/main.dart
@@ -136,15 +136,15 @@ class _MyHomePageState extends State<MyHomePage> {
     // Print image
     final ByteData data = await rootBundle.load('assets/logo.png');
     final Uint8List bytes = data.buffer.asUint8List();
-    final Image image = decodeImage(bytes);
-    printer.image(image);
+    // final Image image = decodeImage(bytes);
+    // printer.image(image);
     // Print image using alternative commands
     // printer.imageRaster(image);
     // printer.imageRaster(image, imageFn: PosImageFn.graphics);
 
     // Print barcode
-    final List<int> barData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 4];
-    printer.barcode(Barcode.upcA(barData));
+    // final List<int> barData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 4];
+    // printer.barcode(Barcode.upcA(barData));
 
     // Print mixed (chinese + latin) text. Only for printers supporting Kanji mode
     // printer.text(
@@ -159,121 +159,121 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> printDemoReceipt(NetworkPrinter printer) async {
     // Print image
+    printer.setGlobalFont(PosFontType.fontA, maxCharsPerLine: 42);
     final ByteData data = await rootBundle.load('assets/rabbit_black.jpg');
     final Uint8List bytes = data.buffer.asUint8List();
     final Image image = decodeImage(bytes);
     printer.image(image);
 
-    printer.text('GROCERYLY',
-        styles: PosStyles(
-          align: PosAlign.center,
-          height: PosTextSize.size2,
-          width: PosTextSize.size2,
-        ),
-        linesAfter: 1);
+    // printer.text('GROCERYLY',
+    //     styles: PosStyles(
+    //       align: PosAlign.center,
+    //       height: PosTextSize.size2,
+    //       width: PosTextSize.size2,
+    //       fontType: PosFontType.fontA
+    //     ),
+    //     linesAfter: 1);
 
-    printer.text('889  Watson Lane', styles: PosStyles(align: PosAlign.center));
-    printer.text('New Braunfels, TX',
-        styles: PosStyles(align: PosAlign.center));
-    printer.text('Tel: 830-221-1234',
-        styles: PosStyles(align: PosAlign.center));
-    printer.text('Web: www.example.com',
-        styles: PosStyles(align: PosAlign.center), linesAfter: 1);
+    // printer.text('889  Watson Lane', styles: PosStyles(align: PosAlign.center));
+    // printer.text('New Braunfels, TX', styles: PosStyles(align: PosAlign.center));
+    // printer.text('Tel: 830-221-1234', styles: PosStyles(align: PosAlign.center));
+    // printer.text('Web: www.example.com', styles: PosStyles(align: PosAlign.center), linesAfter: 1);
+    printer.text('01234567890123456789012345678901234567890123456789', styles: PosStyles(align: PosAlign.left), linesAfter: 1);
 
     printer.hr();
     printer.row([
-      PosColumn(text: 'Qty', width: 1),
-      PosColumn(text: 'Item', width: 7),
       PosColumn(
-          text: 'Price', width: 2, styles: PosStyles(align: PosAlign.right)),
-      PosColumn(
-          text: 'Total', width: 2, styles: PosStyles(align: PosAlign.right)),
-    ]);
-
-    printer.row([
-      PosColumn(text: '2', width: 1),
-      PosColumn(text: 'ONION RINGS', width: 7),
-      PosColumn(
-          text: '0.99', width: 2, styles: PosStyles(align: PosAlign.right)),
-      PosColumn(
-          text: '1.98', width: 2, styles: PosStyles(align: PosAlign.right)),
+          text: '01234567890123456789012345678901234567890123456789', width: 12, styles: PosStyles(align: PosAlign.left)),
     ]);
     printer.row([
-      PosColumn(text: '1', width: 1),
-      PosColumn(text: 'PIZZA', width: 7),
-      PosColumn(
-          text: '3.45', width: 2, styles: PosStyles(align: PosAlign.right)),
-      PosColumn(
-          text: '3.45', width: 2, styles: PosStyles(align: PosAlign.right)),
+      PosColumn(text: '0123456789012', width: 6, styles: PosStyles(align: PosAlign.right)),
+      PosColumn(text: '0123456789012', width: 6, styles: PosStyles(align: PosAlign.right)),
     ]);
-    printer.row([
-      PosColumn(text: '1', width: 1),
-      PosColumn(text: 'SPRING ROLLS', width: 7),
-      PosColumn(
-          text: '2.99', width: 2, styles: PosStyles(align: PosAlign.right)),
-      PosColumn(
-          text: '2.99', width: 2, styles: PosStyles(align: PosAlign.right)),
-    ]);
-    printer.row([
-      PosColumn(text: '3', width: 1),
-      PosColumn(text: 'CRUNCHY STICKS', width: 7),
-      PosColumn(
-          text: '0.85', width: 2, styles: PosStyles(align: PosAlign.right)),
-      PosColumn(
-          text: '2.55', width: 2, styles: PosStyles(align: PosAlign.right)),
-    ]);
-    printer.hr();
-
-    printer.row([
-      PosColumn(
-          text: 'TOTAL',
-          width: 6,
-          styles: PosStyles(
-            height: PosTextSize.size2,
-            width: PosTextSize.size2,
-          )),
-      PosColumn(
-          text: '\$10.97',
-          width: 6,
-          styles: PosStyles(
-            align: PosAlign.right,
-            height: PosTextSize.size2,
-            width: PosTextSize.size2,
-          )),
-    ]);
-
-    printer.hr(ch: '=', linesAfter: 1);
-
-    printer.row([
-      PosColumn(
-          text: 'Cash',
-          width: 8,
-          styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
-      PosColumn(
-          text: '\$15.00',
-          width: 4,
-          styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
-    ]);
-    printer.row([
-      PosColumn(
-          text: 'Change',
-          width: 8,
-          styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
-      PosColumn(
-          text: '\$4.03',
-          width: 4,
-          styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
-    ]);
-
-    printer.feed(2);
-    printer.text('Thank you!',
-        styles: PosStyles(align: PosAlign.center, bold: true));
-
-    final now = DateTime.now();
-    final formatter = DateFormat('MM/dd/yyyy H:m');
-    final String timestamp = formatter.format(now);
-    printer.text(timestamp,
-        styles: PosStyles(align: PosAlign.center), linesAfter: 2);
+    //
+    // printer.row([
+    //   PosColumn(text: '2', width: 1, styles: PosStyles(fontType: PosFontType.fontA)),
+    //   PosColumn(text: 'ONION RINGS', width: 7, styles: PosStyles(fontType: PosFontType.fontA)),
+    //   PosColumn(
+    //       text: '0.99', width: 2, styles: PosStyles(align: PosAlign.right, fontType: PosFontType.fontA)),
+    //   PosColumn(
+    //       text: '1.98', width: 2, styles: PosStyles(align: PosAlign.right,fontType: PosFontType.fontA)),
+    // ]);
+    // printer.row([
+    //   PosColumn(text: '1', width: 1),
+    //   PosColumn(text: 'PIZZA', width: 7),
+    //   PosColumn(
+    //       text: '3.45', width: 2, styles: PosStyles(align: PosAlign.right)),
+    //   PosColumn(
+    //       text: '3.45', width: 2, styles: PosStyles(align: PosAlign.right)),
+    // ]);
+    // printer.row([
+    //   PosColumn(text: '1', width: 1),
+    //   PosColumn(text: 'SPRING ROLLS', width: 7),
+    //   PosColumn(
+    //       text: '2.99', width: 2, styles: PosStyles(align: PosAlign.right)),
+    //   PosColumn(
+    //       text: '2.99', width: 2, styles: PosStyles(align: PosAlign.right)),
+    // ]);
+    // printer.row([
+    //   PosColumn(text: '3', width: 1),
+    //   PosColumn(text: 'CRUNCHY STICKS', width: 7),
+    //   PosColumn(
+    //       text: '0.85', width: 2, styles: PosStyles(align: PosAlign.right)),
+    //   PosColumn(
+    //       text: '2.55', width: 2, styles: PosStyles(align: PosAlign.right)),
+    // ]);
+    // printer.hr();
+    //
+    // printer.row([
+    //   PosColumn(
+    //       text: 'TOTAL',
+    //       width: 6,
+    //       styles: PosStyles(
+    //         height: PosTextSize.size2,
+    //         width: PosTextSize.size2,
+    //       )),
+    //   PosColumn(
+    //       text: '\$10.97',
+    //       width: 6,
+    //       styles: PosStyles(
+    //         align: PosAlign.right,
+    //         height: PosTextSize.size2,
+    //         width: PosTextSize.size2,
+    //       )),
+    // ]);
+    //
+    // printer.hr(ch: '=', linesAfter: 1);
+    //
+    // printer.row([
+    //   PosColumn(
+    //       text: 'Cash',
+    //       width: 8,
+    //       styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    //   PosColumn(
+    //       text: '\$15.00',
+    //       width: 4,
+    //       styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    // ]);
+    // printer.row([
+    //   PosColumn(
+    //       text: 'Change',
+    //       width: 8,
+    //       styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    //   PosColumn(
+    //       text: '\$4.03',
+    //       width: 4,
+    //       styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    // ]);
+    //
+    // printer.feed(2);
+    // printer.text('Thank you!',
+    //     styles: PosStyles(align: PosAlign.center, bold: true));
+    //
+    // final now = DateTime.now();
+    // final formatter = DateFormat('MM/dd/yyyy H:m');
+    // final String timestamp = formatter.format(now);
+    // printer.text(timestamp,
+    //     styles: PosStyles(align: PosAlign.center), linesAfter: 2);
 
     // Print QR Code from image
     // try {
